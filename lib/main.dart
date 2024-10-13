7f5835b42f026b12adcd954a1daa15d99550a262
KASRA10 <kasrasparrow@gmail.comm>
2024-10-13 20:00:59 +0330

    Add Text, Icons And Some Motto

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,151 @@ class _LuckyDiceChangerState extends State<LuckyDiceChanger> {
                 semanticLabel: 'Go To Main Menu, Setting',
               ),
             )
-          ],
+          ], // Action[]
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Every Dice Number Has A Special Meaning Here!!!',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Amaranth',
+                    fontSize: 14,
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                  endIndent: 100,
+                  indent: 100,
+                ),
+                Text(
+                  'All Your Six Dices Would Be Counted',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Amaranth',
+                    fontSize: 12,
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                  endIndent: 150,
+                  indent: 150,
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.green[400],
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Good Luck',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice1.png',
+                    width: 30,
+                    semanticLabel: 'A First Dice Icon',
+                  ),
+                  title: Text(
+                    '1. It is Just Beginning!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice2.png',
+                    width: 30,
+                    semanticLabel: 'A Second Dice Icon',
+                  ),
+                  title: Text(
+                    '2. Even Not Close!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice3.png',
+                    width: 30,
+                    semanticLabel: 'A Third Dice Icon',
+                  ),
+                  title: Text(
+                    '3. Never Give Up!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice4.png',
+                    width: 30,
+                    semanticLabel: 'A Fourth Dice Icon',
+                  ),
+                  title: Text(
+                    '4. a little More!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice5.png',
+                    width: 30,
+                    semanticLabel: 'A Fifth Dice Icon',
+                  ),
+                  title: Text(
+                    '5. Not Today!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'lib/images/dice6.png',
+                    width: 30,
+                    semanticLabel: 'A Sixth Dice Icon',
+                  ),
+                  title: Text(
+                    '6. BOOM!',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Amaranth',
+                      fontSize: 22,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+              ], // columnChildren[]
+            ),
+          ),
         ),
       ),
     );
